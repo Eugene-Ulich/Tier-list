28086db8c9e9d2c15daf579a7a14997a177b6173
xProGenitoRx <em.ulich@gmail.com>
2022-02-07 07:57:28 +0200

    Small patches

--- src/components/tier-list/CreateNewTierList.js
+++ src/components/tier-list/CreateNewTierList.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateNewTierList () {
+    const {name, setName} = useState();
+    const {description, setDescription} = useState();
+    const {images, uploadImages} = useState();
+    function handleNewTierlist (event) {
+        event.preventDefault();
+
+    }
     return (
         <div>
-            <h1>Customs Tier List</h1>
-            <p>Create your own tier-list template</p>
-            <form onSubmit={f=>f}>
-                <input type="text" placeholder="Name your Tier List..." required />
-                <input type="textarea" placeholder="Descripe your Tier List..." />
-                <input type="file" />
+            <h1>Custom Tier List</h1>
+            <p>Create your own tier list template</p>
+            <form onSubmit={handleNewTierlist}>
+                <input value={name} onChange={setName} type="text" placeholder="Name your Tier List..." required /><br />
+                <input value={description} onChange={setDescription} type="textarea" placeholder="Descripe your Tier List..." /><br />
+                <input value={images} onChange={uploadImages} multiple type="file" /><br />
+                <p>{images ? "yes" : "no"}</p>
                 <button type="submit">Save</button>
             </form>
         </div>

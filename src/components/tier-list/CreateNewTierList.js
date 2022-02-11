9a8482164fdd08d856ec01de3b8e9c8d80778b6f
Eugene Ulich <em.ulich@gmail.com>
2022-02-11 17:37:25 +0200

    .

--- src/components/tier-list/CreateNewTierList.js
+++ src/components/tier-list/CreateNewTierList.js
@@ -1,24 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
-export default function CreateNewTierList () {
-    const {name, setName} = useState();
-    const {description, setDescription} = useState();
-    const {images, uploadImages} = useState();
-    function handleNewTierlist (event) {
-        event.preventDefault();
+export default function CreateNewTierList() {
+  const [name, setName] = useState();
+  const [description, setDescription] = useState();
+  const [images, uploadImages] = useState([]);
+  const imageInput = useRef();
+  const handleChange = (stateHandler) => (event) =>
+    stateHandler(event.target.value);
+  function handleNewTierlist(event) {
+    event.preventDefault();
+  }
 
-    }
-    return (
-        <div>
-            <h1>Custom Tier List</h1>
-            <p>Create your own tier list template</p>
-            <form onSubmit={handleNewTierlist}>
-                <input value={name} onChange={setName} type="text" placeholder="Name your Tier List..." required /><br />
-                <input value={description} onChange={setDescription} type="textarea" placeholder="Descripe your Tier List..." /><br />
-                <input value={images} onChange={uploadImages} multiple type="file" /><br />
-                <p>{images ? "yes" : "no"}</p>
-                <button type="submit">Save</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Custom Tier List</h1>
+      <p>Create your own tier list template</p>
+      <form onSubmit={handleNewTierlist}>
+        <input
+          value={name}
+          onChange={handleChange(setName)}
+          type="text"
+          placeholder="Name your Tier List..."
+          required
+        />
+        <br />
+        <input
+          value={description}
+          onChange={handleChange(setDescription)}
+          type="textarea"
+          placeholder="Descripe your Tier List..."
+        />
+        <br />
+        <input
+          ref={imageInput}
+          onChange={handleChange(uploadImages)}
+          multiple
+          type="file"
+        />
+        <br />
+        <p>
+          {imageInput.current ? console.log(imageInput.current.files[0]) : "no"}
+        </p>
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
 }
